ca262101ca46703011ee91a3ff1ed951bf5a5cce
Ahmed Nabil <ahmed.mahran@vodafone.com>
2022-06-10 23:25:18 +0200

    adding more code

--- src/main/kotlin/com/example/plugins/Sockets.kt
+++ src/main/kotlin/com/example/plugins/Sockets.kt
@@ -1,5 +1,6 @@
 package com.example.plugins
 
+import io.ktor.network.sockets.*
 import io.ktor.server.websocket.*
 import io.ktor.websocket.*
 import java.time.Duration
@@ -7,21 +8,38 @@ import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.request.*
 import io.ktor.server.routing.*
+import java.util.*
+import kotlin.collections.LinkedHashSet
+import com.example.Connection
 
 fun Application.configureSockets() {
     install(WebSockets) {
-        pingPeriod = Duration.ofSeconds(15)
-        timeout = Duration.ofSeconds(15)
+        pingPeriod = Duration.ofSeconds(1000)
+        timeout = Duration.ofSeconds(1000)
         maxFrameSize = Long.MAX_VALUE
         masking = false
     }
     routing {
+        val connections = Collections.synchronizedSet<Connection?>(LinkedHashSet())
         webSocket("/chat") {
-            send("You are connected!")
-            for(frame in incoming) {
-                frame as? Frame.Text ?: continue
-                val receivedText = frame.readText()
-                send("You said: $receivedText")
+            println("Adding user!")
+            val thisConnection = Connection(this)
+            connections += thisConnection
+            try {
+                send("You are connected! There are ${connections.count()} users here.")
+                for (frame in incoming) {
+                    frame as? Frame.Text ?: continue
+                    val receivedText = frame.readText()
+                    val textWithUsername = "[${thisConnection.name}]: $receivedText"
+                    connections.forEach {
+                        it.session.send(textWithUsername)
+                    }
+                }
+            } catch (e: Exception) {
+                println(e.localizedMessage)
+            } finally {
+                println("Removing $thisConnection!")
+                connections -= thisConnection
             }
         }
     }
